b5bbe76f745bb1adef4adaf1e3f7b46da0fe59de
Kavi Bidlack <ksbidlack@gmail.com>
2026-06-18 23:29:45 -0700

    add icon files

--- build.rs
+++ build.rs
@@ -1,1 +1,8 @@
-fn main() {}
+fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icons/Proxhy.ico");
+        res.compile().unwrap();
+    }
+}
